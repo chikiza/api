fe05eb223b60d948dbf85e20b3266c11bf897cef
chikiza <83772028+chikiza@users.noreply.github.com>
2024-08-02 23:32:57 -0400

    funciona
    listado de personajes en unicio + link a personajes

---
+++ src/components/Inicio.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react'
+import { todosPersonajes } from '../funciones/funciones'
+
+
+const Inicio = () => {
+    const [personajes, setPersonajes] = useState(null)
+    useEffect(()=>{
+        todosPersonajes(setPersonajes);
+    },[])
+  return (
+    <>
+    {personajes === null ? ('no hay personajes'):(personajes.map(personaje => (
+        <div key={personaje.id}>
+            <a href={`/personaje/${personaje.id}`}>{personaje.name}</a>
+        </div>
+    )))}
+    </>
+  )
+}
+
+export default Inicio
