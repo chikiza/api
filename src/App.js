aba12ce0725201fe9f165a6b8be4464467771120
chikiza <83772028+chikiza@users.noreply.github.com>
2024-08-03 09:36:20 -0400

    grafico
    cargado en inicio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inicio from './components/Inicio'
-import Personaje from './components/Personaje'
-
+import Inicio from './components/Inicio';
+import Personaje from './components/Personaje';
+import Grafico from './components/Grafico';
 
 function App() {
   return (
     <div className="contenedor">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio></Inicio>}></Route>
-        <Route path="/personaje/:id" element={<Personaje></Personaje>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Inicio />
+              <Grafico />
+            </>
+          } />
+          <Route path="/personaje/:id" element={<Personaje />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
